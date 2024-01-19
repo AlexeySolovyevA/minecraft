47bb7492408cc7d50e551aad69c098a884e19c91
Aleksey <mrlehasolovey@ya.ru>
2024-01-19 20:57:48 +0300

    feat: added basic style for slider and arrows

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$('.slider').slick({
+    arrows: true,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    prevArrow:
+    `
+    <button class="slider__arrow slider__arrow--left">
+    <svg width="128" height="128" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M40.5 17L87.5 64" stroke="white" stroke-width="12" stroke-miterlimit="10" stroke-linecap="square"/>
+<path d="M87.5 64L40.5 111" stroke="white" stroke-width="12" stroke-miterlimit="10" stroke-linecap="square"/>
+</svg>
+
+    </button>
+    `,
+    nextArrow: 
+    `
+    <button class="slider__arrow slider__arrow--right">
+    <svg width="128" height="128" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M40.5 17L87.5 64" stroke="white" stroke-width="12" stroke-miterlimit="10" stroke-linecap="square"/>
+<path d="M87.5 64L40.5 111" stroke="white" stroke-width="12" stroke-miterlimit="10" stroke-linecap="square"/>
+</svg>
+
+    </button>
+    `,
+});
