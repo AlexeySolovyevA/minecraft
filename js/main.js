0ffdf6a0b92abfe26859d2775887a21388810623
Aleksey <mrlehasolovey@ya.ru>
2024-01-22 18:20:02 +0300

    feat: styles for arrows

--- js/main.js
+++ js/main.js
@@ -8,9 +8,10 @@ $('.slider').slick({
     `
     <button class="slider__arrow slider__arrow--left">
     <svg width="128" height="128" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M40.5 17L87.5 64" stroke="white" stroke-width="12" stroke-miterlimit="10" stroke-linecap="square"/>
-<path d="M87.5 64L40.5 111" stroke="white" stroke-width="12" stroke-miterlimit="10" stroke-linecap="square"/>
+<path d="M87.5 111L40.5 64" stroke="white" stroke-width="12" stroke-miterlimit="10" stroke-linecap="square"/>
+<path d="M40.5 64L87.5 17" stroke="white" stroke-width="12" stroke-miterlimit="10" stroke-linecap="square"/>
 </svg>
+
 
     </button>
     `,
